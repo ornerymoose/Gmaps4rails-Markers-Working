bb9ef7859f33d175792d3c54cb7d6eafe14193a6
David Pardy <davidpardy@live.com>
2012-04-24 19:40:46 -0500

    app.html.erb issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,27 +15,6 @@
 //= require gmaps4rails/googlemaps.js
 //= require_tree .
 
-// jQuery.ajaxSetup({
-//   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-// })
-// 
-// jQuery.fn.submitWithAjax = function() {
-//   this.submit(function() {
-//     $.post(this.action, (this).serialize(), null, "script");
-// 	return false;
-//   })
-//   return this;
-// };
-// 
-// $(document).ready(function() {
-//   $("#new_map").submitWithAjax();
-// })
-
-// $('#gmaps').live('pageshow',function(event){
-// 
-//   Gmaps.loadMaps(); 
-// });
-
 $(function(){
   
   $("#new_map_form_link").click(function(e) {
